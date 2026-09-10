6020c986c8904f56ad9d1f039ab975248b92f800
sidharth756 <sidharthrk756@gmail.com>
2026-09-10 16:03:17 +0530

    Intial commit - Repo live

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1>Currently under development 🚧!</h1>
+    <p> -sidharth</p>
+    </>
+  )
+}
+
+export default App
